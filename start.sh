9540cedd030ddfa4d1b92f80414ba2fb87e83aa1
pengyi <ryanpoy@gmail.com>
2017-08-21 01:25:19 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ ps aux | grep py | fgrep blog_app | awk '{print "kill -9", $2}' | sh
 sleep 2
 
 cd  /data/apps/blog/current
-py blog_app.py --port=8001 --tmpl=default --debug=false --admin_port=8000 &
-py blog_app.py --port=8002 --tmpl=default --debug=false &
+py blog_app.py --port=8001 --tmpl=default --debug=false --admin_port=8000 >> /data/apps/blog.8001.log &
+py blog_app.py --port=8002 --tmpl=default --debug=false  >> /data/apps/blog.8002.log &
 
