1744feb68326a43959d3cbb4cda6cd9478f6b890
pengyi <ryanpoy@gmail.com>
2019-12-05 12:36:50 +0800

    重构

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ps aux | grep py | fgrep blog_app | awk '{print "kill -9", $2}' | sh
+ps aux | grep python3 | fgrep blog_app | awk '{print "kill -9", $2}' | sh
 
 sleep 2
 
 cd  /data/apps/blog/current
-py blog_app.py --port=8001 --tmpl=default --debug=false --admin_port=8000 >> /data/apps/blog.8001.log &
-py blog_app.py --port=8002 --tmpl=default --debug=false  >> /data/apps/blog.8002.log &
+python3 blog_app.py --port=8001 --tmpl=default --debug=false --admin_port=8000 >> /data/apps/blog.8001.log &
+python3 blog_app.py --port=8002 --tmpl=default --debug=false  >> /data/apps/blog.8002.log &
 
