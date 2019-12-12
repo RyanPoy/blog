91d932c09e252186d5760b6fd1151282f3c0a6d1
pengyi <ryanpoy@gmail.com>
2019-12-12 15:17:35 +0800

    重构：LeftNav的管理

--- app/controllers/admin/ui_partial_controller.py
+++ app/controllers/admin/ui_partial_controller.py
@@ -2,13 +2,24 @@
 from app.controllers import BaseController
 from app.models import *
 from app.libs import *
-from app import ui
 
 
 class LeftNavController(BaseController):
+    
+    LEFT_MENUS = [
+        {
+            'name': '博客管理',
+            'uri': '/admin',
+            'submenus': [
+                { 'name': '文章', 'uri': '/articles' },
+                { 'name': '标签', 'uri': '/tags' },
+                { 'name': '系列', 'uri': '/series' },
+                { 'name': '图片', 'uri': '/images' },
+                { 'name': '单页面', 'uri': '/pages' },
+                { 'name': '友链', 'uri': '/links' },
+            ]
+        }
+    ]
 
     def get(self):
-        # op = self.current_user
-        menus = [ m for m in ui.left_menus() ]
-        return self.end(data={ 'menus': menus } )
-
+        return self.end(data={ 'menus': self.LEFT_MENUS })
--- app/ui.py
+++ app/ui.py
@@ -3,22 +3,6 @@ from tornado.web import UIModule
 from urllib.parse import urlparse
 
 
-left_menus = lambda: [
-    {
-        'name': '博客管理',
-        'uri': '/admin',
-        'submenus': [
-            { 'name': '文章', 'uri': '/articles' },
-            { 'name': '标签', 'uri': '/tags' },
-            { 'name': '系列', 'uri': '/series' },
-            { 'name': '图片', 'uri': '/images' },
-            { 'name': '单页面', 'uri': '/pages' },
-            { 'name': '友链', 'uri': '/links' },
-        ]
-    }
-]
-
-
 class PaginationUI(UIModule):
     """ <div class="pagination font-alt">
           <ul class="pagination">
