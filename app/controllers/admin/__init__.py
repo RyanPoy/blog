bbe69a10a1b4e901e60bac082b8284cab122024c
pengyi <ryanpoy@gmail.com>
2025-07-07 15:40:16 +0800

    use zola

--- app/controllers/admin/__init__.py
+++
@@ -1,9 +0,0 @@
-#coding: utf8
-from .tag_controller import TagController
-from .link_controller import LinkController
-from .series_controller import SeriesController
-from .image_controller import ImageController
-from .page_controller import PageController
-from .article_controller import ArticleController
-from .session_controller import SessionController
-from .ui_partial_controller import LeftNavController
--- app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#coding: utf8
-from .base import *
-from .user import *
-from .tag import *
-from .link import *
-from .image import *
-from .series import *
-from .article import *
-from .page import *
