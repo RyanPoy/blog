af5fcc10dc28045127ca673e0ba04c00664605ee
pengyi <ryanpoy@gmail.com>
2025-07-21 10:26:47 +0800

    fix bug

--- neotale/__init__.py
+++ neotale/__init__.py
@@ -1,22 +1,18 @@
-from mkdocs.themes import Theme
+from mkdocs.theme import BaseTheme
 from mkdocs.utils import log
+from .filters import FILTERS  # 从同级目录的 filters.py 导入你的函数
 
-class NeotaleTheme(Theme):
-    def __init__(self):
-        super().__init__(name='neotale', static_templates=['404.html'])
-        # 添加其他主题初始化配置
-        print("*"*50)
-
-    def get_env(self):
-        # 获取父类的Jinja环境
-        env = super().get_env()
-        
-        # 动态导入自定义过滤器
-        try:
-            from .filters import date_format
-            env.filters['date_format'] = date_format
-            log.debug("Successfully loaded neotale date_format filter")
-        except ImportError as e:
-            log.error(f"Failed to load neotale filters: {e}")
-        
-        return env
+# 创建一个继承自 MkDocs BaseTheme 的新类
+class NeotaleTheme(BaseTheme):
+    """
+    Neotale 主题，内置自定义 filters。
+    """
+    # 重写 extend_jinja_env 方法
+    def extend_jinja_env(self, jinja_env):
+        """
+        在 Jinja2 环境中添加自定义的 filter。
+        """
+        # 'date_format' 是你在模板中使用的 filter 名称
+        # date_format 是你从 filters.py 导入的 Python 函数
+        for name, filter_fn in FILTERS.items():
+            jinja_env.filters[name] = filter_fn
